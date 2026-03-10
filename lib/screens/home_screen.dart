90212540a3c8104b4e1be60b9b7713483d586916
PrincipieCyupe <p.cyubahiro@alustudent.com>
2026-03-11 00:47:33 +0200

    Iniial home screen so far before getting other functionalities done yet

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const Home());
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+       home: HomeContent(),
+       theme: ThemeData(
+        textTheme: GoogleFonts.sourceSans3TextTheme()
+        )
+      );
+  }
+}
+
+class HomeContent extends StatefulWidget {
+  const HomeContent({super.key});
+
+  @override
+  State<HomeContent> createState() => _HomeContentState();
+}
+
+class _HomeContentState extends State<HomeContent> {
+  static List<TextStyle> appstyle = [
+    TextStyle(fontWeight: FontWeight.bold),
+    TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+  ];
+  List<Widget> laterwidgets = [
+    Text("Future home page", style: appstyle[1]),
+    Text("Future Weather page", style: appstyle[1]),
+    Text("Future Market page", style: appstyle[1]),
+    Text("Future Tips and Update page", style: appstyle[1]),
+  ];
+  List<String> items = ["Home", "Weather", "Market", "Tips & Updates"];
+
+  int _selectedIndex = 0;
+  void _onTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      body: Center(child: laterwidgets.elementAt(_selectedIndex)),
+      appBar: AppBar(title: Text("Umuhinzi+", style: appstyle[0]), actions: [Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Image.asset('lib/images/logo.png', width: 20, height: 20,),
+      )],),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: items[0]),
+          BottomNavigationBarItem(icon: Icon(Icons.wb_sunny), label: items[1]),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.trending_up),
+            label: items[2],
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.lightbulb), label: items[3]),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onTapped,
+        unselectedItemColor: Colors.green,
+        selectedItemColor: Colors.orangeAccent,
+      ),
+    );
+  }
+}
