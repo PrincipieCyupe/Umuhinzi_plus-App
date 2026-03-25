5a2eaba866e930449eead0052e03868cc02ebe7f
PrincipieCyupe <p.cyubahiro@alustudent.com>
2026-03-26 00:45:21 +0200

    Updated auth_service

--- lib/features/home/service/auth_service.dart
+++ lib/features/home/service/auth_service.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   User? get currentUser => _auth.currentUser;
 
@@ -16,9 +16,9 @@ class AuthService {
     try {
       final UserCredential credential = await _auth
           .createUserWithEmailAndPassword(
-            email: email.trim(),
-            password: password,
-          );
+        email: email.trim(),
+        password: password,
+      );
       return credential.user;
     } on FirebaseAuthException catch (e) {
       throw _handleAuthException(e);
@@ -42,23 +42,26 @@ class AuthService {
 
   Future<User?> signInWithGoogle() async {
     try {
-      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-
-      if (googleUser == null) {
-        return null;
+      if (kIsWeb) {
+        final GoogleAuthProvider googleProvider = GoogleAuthProvider();
+        final UserCredential userCredential =
+        await _auth.signInWithPopup(googleProvider);
+        return userCredential.user;
       }
 
-      final GoogleSignInAuthentication googleAuth =
-          await googleUser.authentication;
+      await GoogleSignIn.instance.initialize();
+
+      final GoogleSignInAccount googleUser =
+      await GoogleSignIn.instance.authenticate();
+
+      final GoogleSignInAuthentication googleAuth = googleUser.authentication;
 
       final OAuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
 
-      final UserCredential userCredential = await _auth.signInWithCredential(
-        credential,
-      );
+      final UserCredential userCredential =
+      await _auth.signInWithCredential(credential);
 
       return userCredential.user;
     } on FirebaseAuthException catch (e) {
@@ -71,7 +74,10 @@ class AuthService {
   Future<void> signOut() async {
     try {
       await _auth.signOut();
-      await _googleSignIn.signOut();
+
+      if (!kIsWeb) {
+        await GoogleSignIn.instance.signOut();
+      }
     } catch (e) {
       throw Exception("Sign out failed: ${e.toString()}");
     }
