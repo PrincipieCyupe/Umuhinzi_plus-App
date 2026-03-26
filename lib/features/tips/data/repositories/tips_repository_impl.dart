f77b54f10a7f6c6a3a94b585fe59646c125335c7
Keaane <user@example.com>
2026-03-26 03:59:56 -0700

    add fallback to local data if remote fetch fails

--- lib/features/tips/data/repositories/tips_repository_impl.dart
+++ lib/features/tips/data/repositories/tips_repository_impl.dart
@@ -1,16 +1,27 @@
 import '../../domain/entities/tip_entity.dart';
 import '../../domain/repositories/tips_repository.dart';
 import '../datasources/tips_local_data_source.dart';
+import '../datasources/tips_remote_data_source.dart';
 
-// This class actually gets the tips from our local data source
+// This class actually gets the tips from our local or remote data source
 class TipsRepositoryImpl implements TipsRepository {
   final TipsLocalDataSource localDataSource;
+  final TipsRemoteDataSource remoteDataSource;
 
-  TipsRepositoryImpl({required this.localDataSource});
+  TipsRepositoryImpl({
+    required this.localDataSource,
+    required this.remoteDataSource,
+  });
 
   @override
   Future<List<TipEntity>> getTips() async {
-    // Just fetch from local source for now
-    return await localDataSource.getTips();
+    try {
+      // First try to get tips from the real APIs
+      return await remoteDataSource.fetchTips();
+    } catch (e) {
+      // If that fails, we use our local backup list
+      final models = await localDataSource.getTips();
+      return models.toList();
+    }
   }
 }
