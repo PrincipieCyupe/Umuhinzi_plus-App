a29ac86c0243ac83f39bf12fd32b4cc2a97121b2
Joellate <j.teta@alustudent.com>
2026-03-26 21:36:25 -0700

    fix: add dummy TipsRemoteDataSourceImpl to satisfy home DI

--- lib/features/tips/data/repositories/tips_repository_impl.dart
+++ lib/features/tips/data/repositories/tips_repository_impl.dart
@@ -2,12 +2,18 @@ import '../../domain/entities/tip_entity.dart';
 import '../../domain/repositories/tips_repository.dart';
 import '../datasources/tips_local_data_source.dart';
 
+// Dummy class to satisfy home_screen.dart without touching the restricted file
+class TipsRemoteDataSourceImpl {
+  TipsRemoteDataSourceImpl({required dynamic client});
+}
+
 // This class gets the tips from our local data source
 class TipsRepositoryImpl implements TipsRepository {
   final TipsLocalDataSource localDataSource;
 
   TipsRepositoryImpl({
     required this.localDataSource,
+    dynamic remoteDataSource, // Allow unused parameter
   });
 
   @override
